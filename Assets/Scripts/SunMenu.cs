04a41e76c5df0990e17b091e400413b1401882fc
Taro Omiya <taro.omiya@omiyagames.com>
2016-01-30 22:33:44 -0500

    Adding a sun menu.

---
+++ Assets/Scripts/SunMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using OmiyaGames;
+using System;
+
+public class SunMenu : IMenu
+{
+    [SerializeField]
+    GameObject defaultUI;
+    [SerializeField]
+    Transform rotateTransform;
+    [SerializeField]
+    Transform playerTransform;
+
+    public override GameObject DefaultUi
+    {
+        get
+        {
+            return defaultUI;
+        }
+    }
+
+    public override Type MenuType
+    {
+        get
+        {
+            return Type.UnmanagedMenu;
+        }
+    }
+
+    public override void Show(Action<IMenu> stateChanged)
+    {
+        base.Show(stateChanged);
+        playerTransform.rotation = Quaternion.Euler(0, SunRitual.Instance.transform.rotation.eulerAngles.y, 0);
+    }
+
+    void Update ()
+    {
+        rotateTransform.rotation = SunRitual.Instance.LightRotation.rotation;
+    }
+}
