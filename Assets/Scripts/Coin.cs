9ecc41a880c6886f6602df8ca5d4f3823a9eba8d
Taro Omiya <taro.omiya@omiyagames.com>
2016-01-31 01:53:26 -0500

    Adding a win condition.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Coin : MonoBehaviour
+{
+    [SerializeField]
+    GameObject graphics;
+    [SerializeField]
+    ParticleSystem fireworks;
+    [SerializeField]
+    OmiyaGames.SoundEffect sound;
+
+    Collider thisCollider;
+    bool isTriggered = false;
+    CoinCollection collection;
+
+    public CoinCollection Collection
+    {
+        get
+        {
+            return collection;
+        }
+        set
+        {
+            collection = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        thisCollider = GetComponent<Collider>();
+        thisCollider.isTrigger = true;
+
+        isTriggered = false;
+        graphics.SetActive(true);
+        fireworks.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            Trigger(SunRitual.Instance);
+        }
+    }
+    public void Trigger(SunRitual ritual)
+    {
+        if((isTriggered == false) && (Collection != null))
+        {
+            graphics.SetActive(false);
+            fireworks.Play();
+            Collection.RemoveCoin(this);
+            isTriggered = true;
+            sound.Play();
+            ritual.OnCoinCollectionChanged(Collection);
+        }
+    }
+}
